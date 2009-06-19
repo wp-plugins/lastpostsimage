dd703294a8e0d889d74fa5f64df3e374da0e2510
reneade <reneade@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-19 22:57:50 +0000

    added forwarder to fit the stupid wordpress plugins folder structure
    git-svn-id: https://plugins.svn.wordpress.org/lastpostsimage/trunk@127560 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ forwarder.php
@@ -0,0 +1,15 @@
+<?php
+//-----------------------------------------------------------------------------
+/*
+Plugin Name: Last Posts Image
+Version: 0.1.1
+Plugin URI: http://www.rene-ade.de/inhalte/wordpress-plugin-lastpostsimage.html
+Description: This WordPress plugin provides a image always showing the last posts of your blog to use it as signature in communities for example. Go to LastPostsImage in the manage-menu of your adminpanel to customize the picture and get your public link. 
+Author: Ren&eacute; Ade
+Author URI: http://www.rene-ade.de
+*/
+//-----------------------------------------------------------------------------
+?>
+<?php
+  include 'last-posts-image/last-posts-image.php';
+?>
