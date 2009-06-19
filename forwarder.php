3a746a501fb8bfb11055b075199b78c9d5079738
reneade <reneade@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-19 22:59:28 +0000

    updated version number
    git-svn-id: https://plugins.svn.wordpress.org/lastpostsimage/trunk@127561 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- forwarder.php
+++ forwarder.php
@@ -2,7 +2,7 @@
 //-----------------------------------------------------------------------------
 /*
 Plugin Name: Last Posts Image
-Version: 0.1.1
+Version: 0.1.1.1
 Plugin URI: http://www.rene-ade.de/inhalte/wordpress-plugin-lastpostsimage.html
 Description: This WordPress plugin provides a image always showing the last posts of your blog to use it as signature in communities for example. Go to LastPostsImage in the manage-menu of your adminpanel to customize the picture and get your public link. 
 Author: Ren&eacute; Ade
